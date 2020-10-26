7f7af684d3e264950097628f8bf3790b070eeb81
Forrest Reiland <you@example.com>
2020-10-26 14:47:40 -0700

    add spec tests or post route spec

--- spec/requests/post_route_spec.rb
+++ spec/requests/post_route_spec.rb
@@ -18,3 +18,37 @@ describe "post a quote route", :type => :request do
     expect(response).to have_http_status(:created)
   end
 end
+
+describe 'post a quote route exceptions', type: :request do
+  
+  before do
+    post '/quotes', params: { author: 'test_author' }
+  end
+
+  # Tests Header
+  it 'returns 422 invalid status' do
+    expect(response).to have_http_status(422)
+  end
+
+  # Tests Body
+  it 'returns exception message' do
+    expect(JSON.parse(response.body)['message']).to eq("Validation failed: Content can't be blank")
+  end
+end
+
+  describe 'post a quote route exceptions', type: :request do
+  
+    before do
+      post '/quotes', params: { content: 'test_content' }
+    end
+  
+    # Tests Header
+    it 'returns 422 invalid status' do
+      expect(response).to have_http_status(422)
+    end
+  
+    # Tests Body
+    it 'returns exception message' do
+      expect(JSON.parse(response.body)['message']).to eq("Validation failed: Author can't be blank")
+    end
+end

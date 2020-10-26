b494a79e9132bc67f3ddbde58737c36984a8ceb5
Forrest Reiland <you@example.com>
2020-10-26 15:41:37 -0700

    add search code in quote.rb

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,5 @@
 class Quote < ApplicationRecord
-  validates :content, presence: true
-  validates :author, presence: true
+  validates :author, :content, presence: true
 
-  scope :search ->(name_parameter) {where("author ilike ?", "%#{name_parameter}%")}
+  scope :search, -> (name_parameter) { where("author ilike ?", "%#{name_parameter}%") }
 end
